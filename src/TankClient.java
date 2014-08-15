31e0ebf409abee1b63f67e1d909a320bb2006418
ImAWolf <1065395142@qq.com>
2014-08-15 10:34:43 +0800

    TankWar0.4
    使用创缓冲技术，使坦克移动起来

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,9 +1,9 @@
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
 
 public class TankClient {
 	public static void main(String[] args) {
@@ -15,22 +15,22 @@ public class TankClient {
 @SuppressWarnings("serial")
 class TankFrame extends Frame {
 	private int x = 50;
-	private int y = 50;
+	private int y = 50;	
+	private Image offScreenImage = null;
 	
 	public TankFrame() {
 		super("TankWar....");
 	}
-	
+
 	public void launch() {
-		this.setLocation(400,300);
-		this.setSize(400,400);
+		this.setLocation(400, 300);
+		this.setSize(400, 400);
 		this.setVisible(true);
 		this.setResizable(false);
 		this.setBackground(Color.green);
 		new Thread(new PaintThread()).start();
-		
-		
-		this.addWindowListener(new WindowAdapter(){
+
+		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
@@ -42,41 +42,44 @@ class TankFrame extends Frame {
 		Color c = g.getColor();
 		g.setColor(Color.red);
 		g.fillOval(x, y, 30, 30);
-		g.setColor(c);	
-		
+		g.setColor(c);
+
 		x += 5;
 		y += 2;
-	}	
+	}
+	//调用repaint方法时，先调用update方法，再调用paint方法
+	//双缓冲方法
+	@Override
+	public void update(Graphics g) {
+		if(offScreenImage == null) {
+			offScreenImage = this.createImage(400, 400);
+		}
+		//获取画笔
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		//重绘窗口
+		gOffScreen.fillRect(0, 0, 400, 400);
+		gOffScreen.setColor(c);
+		
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+		//offScreenImage = null;
+	}
 	
-	//创建线程，每隔一段时间重画坦克
+	// 创建线程，每隔一段时间重画坦克
 	private class PaintThread implements Runnable {
-
 		@Override
 		public void run() {
-			while(true) {
+			while (true) {
 				repaint();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(50);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
+
 	}
 }
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
