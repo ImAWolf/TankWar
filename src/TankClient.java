b2b242ad329d599f2fbec92245d32da94a3abcdc
ImAWolf <1065395142@qq.com>
2014-08-15 10:00:07 +0800

    TankWar0.2
    画出表示坦克的圆

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -8,15 +10,20 @@ public class TankClient {
 		TankFrame tf = new TankFrame();
 		tf.launch();
 	}
-
 }
 
+@SuppressWarnings("serial")
 class TankFrame extends Frame {
+	public TankFrame() {
+		super("TankWar....");
+	}
+	
 	public void launch() {
 		this.setLocation(400,300);
 		this.setSize(400,400);
 		this.setVisible(true);
 		this.setResizable(false);
+		this.setBackground(Color.green);
 		
 		this.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e) {
@@ -24,5 +31,14 @@ class TankFrame extends Frame {
 			}
 		});
 	}
+
+	@Override
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.red);
+		g.fillOval(50, 50, 30, 30);
+		g.setColor(c);
+	}	
 	
 }
+   
