bd49b9560e7468a316b7d6e93a27635e95cb9482
ImAWolf <1065395142@qq.com>
2014-08-15 10:09:41 +0800

    TankWar0.3
    使用多线程，使坦克动起来

--- src/TankClient.java
+++ src/TankClient.java
@@ -14,6 +14,9 @@ public class TankClient {
 
 @SuppressWarnings("serial")
 class TankFrame extends Frame {
+	private int x = 50;
+	private int y = 50;
+	
 	public TankFrame() {
 		super("TankWar....");
 	}
@@ -24,6 +27,8 @@ class TankFrame extends Frame {
 		this.setVisible(true);
 		this.setResizable(false);
 		this.setBackground(Color.green);
+		new Thread(new PaintThread()).start();
+		
 		
 		this.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e) {
@@ -36,9 +41,42 @@ class TankFrame extends Frame {
 	public void paint(Graphics g) {
 		Color c = g.getColor();
 		g.setColor(Color.red);
-		g.fillOval(50, 50, 30, 30);
-		g.setColor(c);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);	
+		
+		x += 5;
+		y += 2;
 	}	
 	
+	//创建线程，每隔一段时间重画坦克
+	private class PaintThread implements Runnable {
+
+		@Override
+		public void run() {
+			while(true) {
+				repaint();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
 }
    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
