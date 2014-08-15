ea664505e3060aac21b0bd6d9f1ac3f7f574f870
ImAWolf <1065395142@qq.com>
2014-08-15 09:38:21 +0800

    Tank0.12

--- src/TankClient.java
+++ src/TankClient.java
@@ -3,13 +3,16 @@ import java.awt.Frame;
 
 public class TankClient {
 	public static void main(String[] args) {
-		
+		TankFrame tf = new TankFrame();
+		tf.launch();
 	}
 
 }
 
 class TankFrame extends Frame {
 	public void launch() {
-		
+		this.setLocation(400,300);
+		this.setSize(800,600);
+		this.setVisible(true);
 	}
 }
